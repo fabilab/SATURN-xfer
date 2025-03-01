86fbffe8bcd072cad3f34d0091ba751a11ced305
Fabio Zanini <fabio.zanini@unsw.edu.au>
2025-03-01 22:33:02 +1100

    Some boilerplate progress

--- generative.py
+++ generative.py
@@ -1,13 +1,15 @@
-'''
+"""
 Created on Feb 28, 2025
 
 @author: Fabio Zanini
-'''
+"""
+
 import os
-os.environ["OMP_NUM_THREADS"] = "4" # export OMP_NUM_THREADS=4
-os.environ["OPENBLAS_NUM_THREADS"] = "4" # export OPENBLAS_NUM_THREADS=4 
-os.environ["MKL_NUM_THREADS"] = "6" # export MKL_NUM_THREADS=6
-os.environ["VECLIB_MAXIMUM_THREADS"] = "4" # export VECLIB_MAXIMUM_THREADS=4
+
+os.environ["OMP_NUM_THREADS"] = "4"  # export OMP_NUM_THREADS=4
+os.environ["OPENBLAS_NUM_THREADS"] = "4"  # export OPENBLAS_NUM_THREADS=4
+os.environ["MKL_NUM_THREADS"] = "6"  # export MKL_NUM_THREADS=6
+os.environ["VECLIB_MAXIMUM_THREADS"] = "4"  # export VECLIB_MAXIMUM_THREADS=4
 os.environ["NUMEXPR_NUM_THREADS"] = "6"
 
 import os
@@ -16,18 +18,26 @@ import pandas as pd
 from anndata import AnnData
 import warnings
 from builtins import int
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 import losses, miners, distances, reducers, testers
 import torch
 import torch.optim as optim
 import numpy as np
 import utils.logging_presets as logging_presets
 import record_keeper
-from data.gene_embeddings import load_gene_embeddings_adata
-from data.multi_species_data import ExperimentDatasetMulti, multi_species_collate_fn, ExperimentDatasetMultiEqualCT
+from data.gene_embeddings import (
+    load_gene_embeddings_one_species,
+)
+from data.multi_species_data import (
+    ExperimentDatasetMulti,
+    multi_species_collate_fn,
+    ExperimentDatasetMultiEqualCT,
+)
 from data.multi_species_data import ExperimentDatasetMultiEqual
 
-from model.saturn_model import SATURNPretrainModel, SATURNMetricModel, make_centroids
+from model.saturn_model import make_centroids, score_genes_against_centroids
+from model.gen_model import GenerativeModel
 import torch.nn.functional as F
 from tqdm import trange, tqdm
 from pretrain_utils import *
@@ -36,7 +46,8 @@ import argparse
 from datetime import datetime
 from pathlib import Path
 import sys
-sys.path.append('../')
+
+sys.path.append("../")
 
 # SATURN
 from sklearn.cluster import KMeans
@@ -54,26 +65,30 @@ def train(
     optimizer,
     device,
     nepochs,
-    ):
-    '''Train a SATURN-gen model.'''
+    embeddings_tensor=None,
+):
+    """Train a SATURN-gen model.
+
+    embeddings_tensor -- dictionary containing species:protein embeddings
+    """
 
     print("Train new macrogene weights...")
     model.train()
 
-    pbar = tqdm(np.arange(1, nepochs+1))
+    pbar = tqdm(np.arange(1, nepochs + 1))
+    all_ave_loss = []
     for epoch in pbar:
         model.train()
+        epoch_ave_loss = []
 
         for batch_idx, batch_tuple in enumerate(train_loader):
             # Obviously, each minibatch is used independently to speed up convergence
             optimizer.zero_grad()
-            batch_loss = 0
 
             (data, labels, refs, batch_labels) = batch_tuple
-            
+
             if data is None:
                 continue
-
 
             data, labels, refs = data.to(device), labels.to(device), refs.to(device)
 
@@ -86,5 +101,142 @@ def train(
                 px_rs = px_rs.unsqueeze(0)
             if px_drops.dim() != 2:
                 px_drops = px_drops.unsqueeze(0)
-            
+
             gene_weights = model.p_weights.exp()
+
+            # NOTE: the loss has three components:
+            # - the vae loss (ZINB adhaerence)
+            # - a LASSO on the gene -> macrogene weights
+            # - a rank loss on the gene -> macrogene weights
+            # The last two are ok. The first one, let's take a look whether any change is needed given this is generated,
+            # therefore there is no "library size" per se.
+            l = model.loss_vae(
+                data, None, None, 0, px_rates, px_rs, px_drops
+            )  # This loss also works for non vae loss
+            spec_loss = l["loss"] / data.shape[0]
+            l1_loss = model.l1_penalty * model.lasso_loss(model.p_weights.exp())
+            rank_loss = model.pe_sim_penalty * model.gene_weight_ranking_loss(
+                model.p_weights.exp(), embeddings_tensor
+            )
+
+            batch_loss = spec_loss + l1_loss + rank_loss
+
+            epoch_ave_loss.append(float(spec_loss.detach().cpu()))
+
+            # TODO: why is there zero_grad here as well?
+            optimizer.zero_grad()
+            batch_loss.backward()
+            optimizer.step()
+
+        # This looks like a legit reporting bug
+        loss_string = [
+            f"Epoch {epoch}: L1 Loss {l1_loss.detach()} Rank Loss {rank_loss.detach()}"
+        ]
+        loss_string += [f"Avg Loss: {round(np.average(epoch_ave_loss))}"]
+        loss_string = ", ".join(loss_string)
+        pbar.set_description(loss_string)
+        all_ave_loss.append(np.mean(epoch_ave_loss))
+
+        return model
+
+
+def trainer(args):
+    """
+    Runs the generative pipeline
+    """
+    device = torch.device(args.device)
+    species = args.species
+    embedding_path = args.protein_embedding_path
+
+    print("Load the macrogene expression data, embedded by the autoencoder")
+    adata_embed = sc.read(args.adata_embed)
+    print("Loaded macrogene expression data")
+
+    if args.species_guide is not None:
+        species_guide = args.species_guide
+        adata_embed = adata_embed[adata_embed.obs["species"] == species_guide]
+    else:
+        species_guide = "unknown"
+
+    print("Load the protein embeddings for the new species")
+    embedding_dict = load_gene_embeddings_one_species(
+        species=species,
+        embedding_path=embedding_path,
+        embedding_model=args.embedding_model,
+    )
+    # NOTE: the order of genes in the new species is arbitrary
+    all_gene_names = list(embedding_dict.keys())
+    # stacked embeddings
+    X = torch.stack(
+        [embedding_dict[gene_symbol.lower()] for gene_symbol in all_gene_names]
+    )
+    print("Loaded protein embeddings")
+
+    # NOTE: there is no such thing as HVG strictu sensu. We can do HVG after we get the first pass at generating a synthetic atlas
+
+    print("Load the centroids")
+    with open(args.centroids_init_path, "rb") as f:
+        tmp = pickle.load(f)
+        centroids_coords = tmp["centroids"]
+        centroid_score_func = tmp.get("score_func", args.centroid_score_func)
+    print("Loaded centroids and existing scores")
+
+    print("Score genes of new species against cluster centers")
+    centroids_coords = torch.tensor(centroids_coords).type("f4")
+    gene_scores = score_genes_against_centroids(
+        X,
+        centroids_coords,
+        all_gene_names,
+        score_function=centroid_score_func,
+    )
+    centroid_weights = torch.stack(
+        [torch.tensor(species_genes_scores[gn]) for gn in all_gene_names]
+    )
+
+    print("***STARTING GENERATIVE LEARNING***")
+    pretrain_state_dict = torch.load(args.pretrain_model_path)
+    gen_state_dict = {}
+    gen_state_dict["cl_layer_norm.weight"] = deepcopy(
+        pretrain_state_dict["cl_layer_norm.weight"]
+    )
+    gen_state_dict["cl_layer_norm.bias"] = deepcopy(
+        pretrain_state_dict["cl_layer_norm.bias"]
+    )
+    # Copy the encoder parameters from either the pretrain or the metric model
+    if args.encoder == "pretrain":
+        encoder_state_dict = pretrain_state_dict
+    else:
+        encoder_state_dict = torch.load(args.metric_model_path)
+    for key, val in encoder_state_dict.items():
+        if key.startswith("encoder"):
+            gen_state_dict[key] = deepcopy(val)
+
+    gen_model = GenerativeModel(
+        species=species,
+        gene_scores,
+        hidden_dim=xfer_state_dict["encoder.0.0.bias"].shape[0],
+        embed_dim=xfer_state_dict["encoder.1.1.bias"].shape[0],
+        dropout=0.1,
+    )
+    gen_model.load_state_dict(gen_state_dict)
+    gen_model.to(device)
+
+    # Set a few infra things
+    args.dir_ = (
+        args.work_dir
+        + args.log_dir
+        + "test"
+        + str(args.model_dim)
+        + "_data_"
+        + args.species
+        + ("_org_" + str(args.org) if args.org is not None else "")
+        + ("_" + dt if args.time_stamp else "")
+        + ("_" + args.tissue_subset if args.tissue_subset else "")
+        + ("_guide_" + args.guide_species if args.guide_species else "")
+        + ("_seed_" + str(args.seed))
+    )
+    metric_dir = Path(args.work_dir)
+    metric_dir.mkdir(parents=True, exist_ok=True)
+    run_name = args.dir_.split(args.log_dir)[-1]
+
+    print("***ZERO-SHOT GENERATION***")
--- model/gen_model.py
+++ model/gen_model.py
@@ -1,13 +1,14 @@
-'''
+"""
 Created on Feb 28, 2025
 
 @author: Fabio Zanini
-'''
+"""
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from scvi.distributions import ZeroInflatedNegativeBinomial
+
 try:
     from kmeans_pytorch import kmeans
 except ImportError:
@@ -18,12 +19,10 @@ import scanpy as sc
 import numpy as np
 
 
-from .saturn_model import (
-    full_block
-)
+from .saturn_model import full_block
 
 
-class SATURNGenModel(torch.nn.Module):
+class GenerativeModel(torch.nn.Module):
     def __init__(
         self,
         gene_scores,
@@ -34,46 +33,48 @@ class SATURNGenModel(torch.nn.Module):
         l1_penalty=0.1,
         pe_sim_penalty=1.0,
     ):
-        '''Generate synthetic cell gene expression vectors for new species.'''
+        """Generate synthetic cell gene expression vectors for new species."""
         super.__init__()
 
         self.num_gene_scores = len(gene_scores)
         self.p_weights = nn.Parameter(gene_scores.float().t().log())
-        if random_weights: # for the genes to centroids weights
-            nn.init.xavier_uniform_(self.p_weights, gain=nn.init.calculate_gain('relu'))
+        if random_weights:  # for the genes to centroids weights
+            nn.init.xavier_uniform_(self.p_weights, gain=nn.init.calculate_gain("relu"))
 
         # num_cl is the number of macrogenes
         self.num_cl = gene_scores.shape[1]
-        self.expr_filler = nn.Parameter(torch.zeros(self.num_genes), requires_grad=False) # pad exprs with zeros        
-
+        self.expr_filler = nn.Parameter(
+            torch.zeros(self.num_genes), requires_grad=False
+        )  # pad exprs with zeros
 
         # Z Encoder
         self.encoder = nn.Sequential(
-                full_block(self.num_cl, self.hidden_dim, self.dropout),
-                full_block(self.hidden_dim, self.embed_dim, self.dropout),
+            full_block(self.num_cl, self.hidden_dim, self.dropout),
+            full_block(self.hidden_dim, self.embed_dim, self.dropout),
         )
 
         self.px_decoder = nn.Sequential(
             full_block(self.embed_dim, self.hidden_dim, self.dropout),
         )
-        
+
         self.cl_scale_decoder = full_block(self.hidden_dim, self.num_cl)
-        
+
         self.px_dropout_decoder = nn.Sequential(
-                nn.Linear(self.hidden_dim, gene_idxs[1] - gene_idxs[0]) 
+            nn.Linear(self.hidden_dim, gene_idxs[1] - gene_idxs[0])
         )
-        
+
         self.px_r = torch.nn.Parameter(torch.randn(gene_idxs[1] - gene_idxs[0]))
 
         # Gene to Macrogene modifiers
         self.l1_penalty = l1_penalty
         self.pe_sim_penalty = pe_sim_penalty
-        
-        self.p_weights_embeddings = nn.Sequential(
-            full_block(self.num_cl, 256, self.dropout) # This embedding layer will be used in metric learning to encode
-                                                       # similarity in the protein embedding space
-        )
 
+        self.p_weights_embeddings = nn.Sequential(
+            full_block(
+                self.num_cl, 256, self.dropout
+            )  # This embedding layer will be used in metric learning to encode
+            # similarity in the protein embedding space
+        )
 
     def forward(self, inp):
         """The actual layering of the model.
@@ -99,7 +100,7 @@ class SATURNGenModel(torch.nn.Module):
         expr = torch.log1p(expr)
         expr = expr.unsqueeze(1)
         clusters = []
-        expr_and_genef = expr 
+        expr_and_genef = expr
         x = nn.functional.linear(expr_and_genef.squeeze(), self.p_weights.exp())
         x = self.cl_layer_norm(x)
         x = F.relu(x)
@@ -111,19 +112,18 @@ class SATURNGenModel(torch.nn.Module):
         if encoded.dim() != 2:
             encoded = encoded.unsqueeze(0)
 
-
         decoded = self.px_decoder(encoded)
         library = torch.log(inp.sum(1)).unsqueeze(1)
         cl_scale = self.cl_scale_decoder(decoded)
 
         # Decode macrogenes to genes of this one NEW species
-        cl_to_px = nn.functional.linear(cl_scale.unsqueeze(0), self.p_weights.exp().t())[:, :, idx[0]:idx[1]]
-
+        cl_to_px = nn.functional.linear(
+            cl_scale.unsqueeze(0), self.p_weights.exp().t()
+        )[:, :, idx[0] : idx[1]]
 
         # Distribute the means by cluster, based on the current weights of gene_to_macrogene
         px_scale_decode = nn.Softmax(-1)(cl_to_px.squeeze())
-        px_rate =  torch.exp(library) * px_scale_decode
-
+        px_rate = torch.exp(library) * px_scale_decode
 
         # Dropout decoder for this one species, used to get the expected dropout rate in ZINB
         px_drop = self.px_dropout_decoder(decoded)
@@ -134,11 +134,59 @@ class SATURNGenModel(torch.nn.Module):
         return encoder_input, encoded, px_rate, px_r, px_drop
 
     def get_reconstruction_loss(self, x, px_rate, px_r, px_dropout):
-        '''https://github.com/scverse/scvi-tools/blob/main/src/scvi/module/_vae.py
+        """https://github.com/scverse/scvi-tools/blob/main/src/scvi/module/_vae.py
 
 
         NOTE: whether negative binomimals are a good idea for this is questionable.
-        '''
-        return -ZeroInflatedNegativeBinomial(
-            mu=px_rate, theta=px_r, zi_logits=px_dropout
-        ).log_prob(x).sum(dim=-1)
+        """
+        return (
+            -ZeroInflatedNegativeBinomial(mu=px_rate, theta=px_r, zi_logits=px_dropout)
+            .log_prob(x)
+            .sum(dim=-1)
+        )
+
+    def loss_vae(
+        self, inp, mu, log_var, kld_weight, px_rate, px_r, px_drop, weights=None
+    ):
+        if weights is None:
+            recons_loss = torch.sum(
+                self.get_reconstruction_loss(inp, px_rate, px_r, px_drop)
+            )
+        else:
+            recons_loss = torch.sum(
+                self.get_reconstruction_loss(inp, px_rate, px_r, px_drop) * weights
+            )  # weight by CT abundancy
+
+        loss = recons_loss
+        if self.vae:
+            kld_loss = torch.sum(
+                -0.5 * torch.sum(1 + log_var - mu**2 - log_var.exp(), dim=1), dim=0
+            )
+
+            loss = loss + (kld_weight * kld_loss)
+            return {
+                "loss": loss,
+                "Reconstruction_Loss": recons_loss.detach(),
+                "KLD": -kld_loss.detach(),
+            }
+        else:
+            return {"loss": loss}
+
+    def lasso_loss(self, weights):
+        """Lasso Loss used to regularize (sparsify) the gene to macrogene weights"""
+        loss = torch.nn.L1Loss(reduction="sum")
+        return loss(weights, torch.zeros_like(weights))
+
+    def gene_weight_ranking_loss(self, weights, embeddings):
+        """Ranking loss used to regularize the gene to macrogene weights"""
+        # weights is M x G
+        x1 = self.p_weights_embeddings(weights.t())
+        # genes x 256
+        loss = nn.MSELoss(reduction="sum")
+        similarity = torch.nn.CosineSimilarity()
+
+        idx1 = torch.randint(low=0, high=x1.shape[0], size=(x1.shape[0],))
+        x2 = x1[idx1, :]
+        target = similarity(embeddings, embeddings[idx1, :])
+
+        return loss(similarity(x1, x2), target)
