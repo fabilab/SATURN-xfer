b5088fa27245616483cc12ef5ced2baf47586904
Fabio Zanini <fabio.zanini@unsw.edu.au>
2025-03-02 09:28:22 +1100

    Skeleton for generative file almost there

--- generative.py
+++ generative.py
@@ -146,17 +146,17 @@ def trainer(args):
     """
     device = torch.device(args.device)
     species = args.species
-    embedding_path = args.protein_embedding_path
+    embedding_path = args.in_embeddings_path
 
     print("Load the macrogene expression data, embedded by the autoencoder")
-    adata_embed = sc.read(args.adata_embed)
+    adata_embed = sc.read(args.in_adata_path)
     print("Loaded macrogene expression data")
 
     if args.species_guide is not None:
         species_guide = args.species_guide
         adata_embed = adata_embed[adata_embed.obs["species"] == species_guide]
     else:
-        species_guide = "unknown"
+        species_guide = "all"
 
     print("Load the protein embeddings for the new species")
     embedding_dict = load_gene_embeddings_one_species(
@@ -240,3 +240,343 @@ def trainer(args):
     run_name = args.dir_.split(args.log_dir)[-1]
 
     print("***ZERO-SHOT GENERATION***")
+    zeroshot_macrogenes = adata_embed.obsm["macrogenes"]
+
+    # TODO: invert from macrogene to gene space
+    zeroshot_counts = None
+
+    zeroshot_species = species
+    zeroshot_lab = adata_embed.obs["truth_labels"].values
+    zeroshot_ref = adata_embed.obs["ref_labels"].values
+    all_obs_names = adata_embed.obs_names
+
+    adata_zeroshot = create_output_anndata(
+        zeroshot_counts,
+        zeroshot_lab,
+        zeroshot_species,
+        zeroshot_macrogenes.cpu().numpy(),
+        zeroshot_ref,
+        obs_names=all_obs_names,
+    )
+    adata_zeroshot.obs["species"] = species
+    if species_guide is not None:
+        adata_zeroshot.uns["guide_species"] = species_guide
+
+    if len(run_name) > 70:
+        zeroshot_adata_fn = "zeroshot.h5ad"
+    else:
+        zeroshot_adata_fn = f"{run_name}_zeroshot.h5ad"
+    zeroshot_adata_path = metric_dir / zeroshot_adata_fn
+    adata_zeroshot.write(zeroshot_adata_path)
+
+    # If no training is requested, we can stop here
+    if not args.train:
+        return
+
+
+    # Make the guide species data loader
+    if use_batch_labels:  # we have a batch column to use for the pretrainer
+        train_dataset = ExperimentDatasetMultiEqual(
+            all_data=species_to_adata_xfer,
+            all_ys={
+                species: adata.obs["truth_labels"]
+                for (species, adata) in species_to_adata_xfer.items()
+            },
+            all_refs={
+                species: adata.obs["ref_labels"]
+                for (species, adata) in species_to_adata_xfer.items()
+            },
+            all_batch_labs={
+                species: adata.obs["batch_labels"]
+                for (species, adata) in species_to_adata_xfer.items()
+            },
+        )
+    else:
+        train_dataset = ExperimentDatasetMultiEqual(
+            all_data=species_to_adata_xfer,
+            all_ys={
+                species: adata.obs["truth_labels"]
+                for (species, adata) in species_to_adata_xfer.items()
+            },
+            all_refs={
+                species: adata.obs["ref_labels"]
+                for (species, adata) in species_to_adata_xfer.items()
+            },
+            all_batch_labs={},
+        )
+    # Load data with shuffling
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        collate_fn=multi_species_collate_fn,
+        batch_size=1024,
+        shuffle=True,
+    )
+
+    # TODO: freeze only the parameters that need no retraining (common encoders/decoders)
+    # The species-specific parts should be obviously unfrozen
+    for parameter in gen_model.parameters():
+        parameter.requires_grad = False
+    gen_model.p_weights.requires_grad = True
+
+    # Create the optimizer
+    # lr is the learning rate for the metric model
+    optimizer = optim.Adam(gen_model.parameters(), lr=args.lr)
+
+    distance = distances.CosineSimilarity()
+    reducer = reducers.ThresholdReducer(low=0)
+
+    print("***STARTING FINE-TUNING***")
+    # TODO: do something about estimating HVGs in the new species
+    train(
+        gen_model, train_loader, optimizer, device, args.epochs, embeddings_tensor=X,
+    )
+
+    if args.gen_model_path is not None:
+        # Save the transfer model if asked to
+        print(f"Saving trained Generative Model to {args.gen_model_path}")
+        torch.save(gen_model.state_dict(), args.gen_model_path)
+
+    print("***FINE-TUNED INFERENCE***")
+    if use_batch_labels:
+        (
+            train_emb,
+            train_lab,
+            train_species,
+            train_macrogenes,
+            train_ref,
+            train_batch,
+        ) = get_all_embeddings(
+            gen_dataset,
+            gen_model,
+            device,
+            use_batch_labels,
+        )
+        adata_finetuned = create_output_anndata(
+            train_emb,
+            train_lab,
+            train_species,
+            train_macrogenes.cpu().numpy(),
+            train_ref,
+            use_batch_labels,
+            train_batch,
+            obs_names=all_obs_names,
+        )
+    else:
+        train_emb, train_lab, train_species, train_macrogenes, train_ref = (
+            get_all_embeddings(
+                gen_dataset,
+                gen_model,
+                device,
+                use_batch_labels,
+            )
+        )
+        adata_finetuned = create_output_anndata(
+            train_emb,
+            train_lab,
+            train_species,
+            train_macrogenes.cpu().numpy(),
+            train_ref,
+            obs_names=all_obs_names,
+        )
+    adata_finetuned.obs["species"] = args.species
+    if species_guide is not None:
+        adata_finetuned.uns["guide_species"] = species_guide
+    adata_finetuned.uns["gen_training_epoch"] = args.epochs
+
+    if len(run_name) > 70:
+        final_adata_fn = f"epoch_{args.epochs}_finetuned_adata.h5ad"
+    else:
+        final_adata_fn = f"{run_name}_epoch_{args.epochs}_finetuned.h5ad"
+    final_path = metric_dir / final_adata_fn
+    adata_finetuned.write(final_path)
+    print(f"Fine-tuned AnnData Path: {final_path}")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Set model hyperparametrs.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    # Run Setup
+    parser.add_argument(
+        "--in_adata_path",
+        type=str,
+        required=True,
+        help="Path to h5ad of the data in macrogene and embedding space, used to generate synthetic cell profiles.",
+    )
+    parser.add_argument(
+        "--in_embeddings_path",
+        type=str,
+        required=True,
+        help="Path to embeddings summary pt file for the peptides of the species to infer",
+    )
+    parser.add_argument(
+        "--training_csv_path",
+        type=str,
+        required=True,
+        help="Path to csv containing training adata and embedding paths and species names",
+    )
+    parser.add_argument(
+        "--species", type=str, help="Data to infer belongs to this species (optional)"
+    )
+    parser.add_argument(
+        "--guide_species",
+        type=str,
+        help="Choose a guide species to generate synthetic cell profiles from. Default is to generate profiles from all species.",
+    )
+    parser.add_argument("--device", type=str, help="Set GPU/CPU")
+    parser.add_argument("--device_num", type=int, help="Set GPU Number")
+    parser.add_argument("--time_stamp", type=bool, help="Add time stamp in file name")
+    parser.add_argument("--org", type=str, help="Add organization to filename")
+    parser.add_argument("--log_dir", type=str, help="Log directory")
+    parser.add_argument("--work_dir", type=str, help="Working directory")
+    parser.add_argument("--seed", type=int, help="Init Seed")
+    parser.add_argument("--in_label_col", type=str, help="Label column for input data")
+    parser.add_argument(
+        "--ref_label_col", type=str, help="Reference label column for input data"
+    )
+    parser.add_argument(
+        "--tissue_subset",
+        type=str,
+        help="Subset the input anndatas by the column args.tissue_column to just be this tissue",
+    )
+    parser.add_argument(
+        "--tissue_column",
+        type=str,
+        help="When subsetting the input anndatas by the column, use this column name.",
+    )
+
+    # SATURN Setup
+    parser.add_argument("--hv_genes", type=int, help="Number of highly variable genes")
+    parser.add_argument(
+        "--hv_span",
+        type=float,
+        help="Fraction of cells to use when calculating highly variable genes, scanpy defeault is 0.3.",
+    )
+    parser.add_argument(
+        "--centroids_init_path",
+        type=str,
+        required=True,
+        help="Path to existing centroids pretraining weights, or location to save to.",
+    )
+    parser.add_argument(
+        "--embedding_model",
+        type=str,
+        choices=["ESM1b", "MSA1b", "protXL", "ESM1b_protref", "ESM2", "ESMc"],
+        help="Gene embedding model whose embeddings should be loaded if using gene_embedding_method",
+    )
+    parser.add_argument(
+        "--centroid_score_func",
+        type=str,
+        choices=["default", "one_hot", "smoothed"],
+        help="Gene embedding model whose embeddings should be loaded if using gene_embedding_method",
+    )
+
+    # Model Setup
+    parser.add_argument(
+        "--hidden_dim", type=int, help="Model first layer hidden dimension"
+    )
+    parser.add_argument("--model_dim", type=int, help="Model latent space dimension")
+    parser.add_argument(
+        "--random_weights",
+        action="store_true",
+        help="Initialise the top layer with random weights.",
+    )
+
+    # Metric Learning Arguments
+    parser.add_argument(
+        "--use_ref_labels",
+        type=bool,
+        nargs="?",
+        const=True,
+        help="Use reference labels when aligning",
+    )
+    parser.add_argument("--batch_size", type=int, help="batch size")
+    parser.add_argument(
+        "--encoder",
+        type=str,
+        choices=["pretrain", "metric"],
+        help="Which encoder to use for the transfer model. Default is to use the final metric model.",
+    )
+    parser.add_argument(
+        "--train",
+        action="store_true",
+        help="Train the transfer model instead of zero-shot inference.",
+    )
+    parser.add_argument("--epochs", type=int, help="How many epochs to train for")
+
+
+    # Model paths
+    parser.add_argument(
+        "--pretrain_model_path",
+        type=str,
+        required=True,
+        help="Path to load a pretraining model from",
+    )
+    parser.add_argument(
+        "--metric_model_path",
+        type=str,
+        required=True,
+        help="Path to load a metric (macrogene -> embedding) model from",
+    )
+    parser.add_argument(
+        "--gen_model_path",
+        type=str,
+        help="Path to store the transfer learning model to",
+    )
+
+    # Defaults
+    parser.set_defaults(
+        org=None,
+        species="new",
+        guide_species=None,
+        in_label_col=None,
+        ref_label_col="CL_class_coarse",
+        non_species_batch_col=None,
+        device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+        device_num=0,
+        batch_size=4096,
+        model_dim=256,
+        hidden_dim=256,
+        hv_genes=8000,
+        epochs=50,
+        metric_lr=0.001,
+        pretrain_epochs=200,
+        log_dir="tboard_log/",
+        work_dir="./out/",
+        time_stamp=False,
+        mnn=True,
+        pretrain=True,
+        vae=False,
+        use_ref_labels=False,
+        embedding_model="ESM1b",
+        gene_embedding_method=None,
+        centroids_init_path=None,
+        seed=0,
+        score_ref_labels=False,
+        tissue_subset=None,
+        tissue_column="tissue_type",
+        hv_span=0.3,
+        centroid_score_func="default",
+        train=False,
+        encoder="metric",
+    )
+
+    args = parser.parse_args()
+    torch.cuda.set_device(args.device_num)
+    print(f"Using Device {args.device_num}")
+
+    # Numpy seed
+    np.random.seed(args.seed)
+    # Torch Seed
+    torch.manual_seed(args.seed)
+    # Default random seed
+    random.seed(args.seed)
+
+    print(f"Set seed to {args.seed}")
+    # Don't Balance the species numbers
+    ExperimentDatasetMultiEqual = ExperimentDatasetMulti
+
+    trainer(args)
